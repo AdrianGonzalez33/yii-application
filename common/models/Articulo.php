282b32e5811126567fa36145485a0bf8607ac288
AdrianGonzalez33 <48592565+AdrianGonzalez33@users.noreply.github.com>
2020-02-27 13:04:13 +0100

    27/02/2020 cambios Forms y gii

---
+++ common/models/Articulo.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "articulo".
+ *
+ * @property int $id_articulo
+ * @property string $titulo
+ * @property string $contenido
+ * @property string $autor
+ */
+class Articulo extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'articulo';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['titulo', 'contenido', 'autor'], 'required'],
+            [['titulo', 'autor'], 'string', 'max' => 50],
+            [['contenido'], 'string', 'max' => 250],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id_articulo' => 'Id Articulo',
+            'titulo' => 'Titulo',
+            'contenido' => 'Contenido',
+            'autor' => 'Autor',
+        ];
+    }
+}
